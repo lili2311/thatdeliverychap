3789a066c1c69ff8f183824dbe23fb477e72fc27
Liliana Kastilio <kastilio.liliana@gmail.com>
2014-12-14 08:43:55 -0600

    Added all the html pages, created a logo, chaned the styling.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 var mozjpeg = require('imagemin-mozjpeg');
 
-grunt.loadNpmTasks('grunt-aws-s3');
+//grunt.loadNpmTasks('grunt-aws-s3');
 grunt.loadNpmTasks('grunt-compass'); 
 grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -17,62 +17,62 @@ grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 grunt.initConfig({
 pkg: grunt.file.readJSON('package.json'),
-s3settings: grunt.file.readJSON('s3settings.json'),
+// s3settings: grunt.file.readJSON('s3settings.json'),
 
-aws_s3: {
-  options: {
-    accessKeyId: '<%= s3settings.key %>', // Use the variables
-    secretAccessKey: '<%= s3settings.secret %>', // You can also use env variables
-    region: '<%= s3settings.region %>',
-    uploadConcurrency: 5, // 5 simultaneous uploads
-    downloadConcurrency: 5 // 5 simultaneous downloads
-  },
-  live: {
-    options: {
-      bucket: '<%= s3settings.bucket %>',
-      differential: false ,// Only uploads the files that have changed
-      debug: false,
-/**      params: {
-        ContentEncoding: 'gzip', // applies to all the files!
-        CacheControl: 'max-age=290304000, public',
-      }**/
-    },
-    files: [
-      {expand: true, cwd: 'src/', src: ['**'], dest: ''},
-    ]
-  },
-  download: {
-    options: {
-      bucket: '<%= s3settings.bucket %>',
-    },
-    files: [
-      {dest: '/', cwd: 'backup/', action: 'download'},
-    ]
-  }
-},
+// aws_s3: {
+//   options: {
+//     accessKeyId: '<%= s3settings.key %>', // Use the variables
+//     secretAccessKey: '<%= s3settings.secret %>', // You can also use env variables
+//     region: '<%= s3settings.region %>',
+//     uploadConcurrency: 5, // 5 simultaneous uploads
+//     downloadConcurrency: 5 // 5 simultaneous downloads
+//   },
+//   live: {
+//     options: {
+//       bucket: '<%= s3settings.bucket %>',
+//       differential: false ,// Only uploads the files that have changed
+//       debug: false,
+// /**      params: {
+//         ContentEncoding: 'gzip', // applies to all the files!
+//         CacheControl: 'max-age=290304000, public',
+//       }**/
+//     },
+//     files: [
+//       {expand: true, cwd: 'src/', src: ['**'], dest: ''},
+//     ]
+//   },
+//   download: {
+//     options: {
+//       bucket: '<%= s3settings.bucket %>',
+//     },
+//     files: [
+//       {dest: '/', cwd: 'backup/', action: 'download'},
+//     ]
+//   }
+// },
 copy: {
   main: {
     files: [
       // includes files within path
-      {expand: false, src: ['*.html'], dest: 'src/', filter: 'isFile'}
+      {expand: false, src: ['*.css'], dest: 'src/css/', filter: 'isFile'}
     ]
   }
 },
-compass: {
-  dist: {
-    options: {
-      sassDir: 'assets/scss',
-      cssDir: 'src/assets/css'
-    }
-  }
-},
+// compass: {
+//   dist: {
+//     options: {
+//       sassDir: 'src/sass',
+//       cssDir: 'src/css'
+//     }
+//   }
+// },
 imagemin: {                          // Task
   dynamic: {                         // Another target
     files: [{
       expand: true,                  // Enable dynamic expansion
-      cwd: 'assets/img/',                   // Src matches are relative to this path
+      cwd: 'src/img/',                   // Src matches are relative to this path
       src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-      dest: 'src/img/'                  // Destination path prefix
+      dest: 'deploy/img/'                  // Destination path prefix
     }]
   }
 },
@@ -81,14 +81,14 @@ concat: {
     separator: ';'
   },
   dist: {
-    src: ['assets/js/min.js','assets/js/map.js'],
-    dest: 'src/assets/js/<%= pkg.name %>.js'
+    src: ['src/js/scroll.js','src/js/map.js'],
+    dest: 'deploy/js/script.js'
   }
 },
 cssmin: {
   combine: {
     files: {
-      'src/assets/css/style.css': ['src/assets/css/style.css']
+      'src/css/style.css': ['deploy/css/style.css']
     }
   }
 },
@@ -99,24 +99,23 @@ htmlmin: {                                     // Task
       collapseWhitespace: true
     },
     files: {                                   // Dictionary of files
-      'src/index.html': 'src/index.html',     // 'destination': 'source'
-      'src/services.html': 'src/services.html',     // 'destination': 'source'
-      'src/contact.html': 'src/contact.html',     // 'destination': 'source'
-      'src/canvascovering.html': 'src/canvascovering.html',     // 'destination': 'source'
-      'src/laminating.html': 'src/laminating.html',     // 'destination': 'source'
-      'src/rebinding.html': 'src/rebinding.html'     // 'destination': 'source'
+      'src/index.html': 'delpoy/index.html',     // 'destination': 'source'
+      'src/services.html': 'deploy/services.html',     // 'destination': 'source'
+      'src/contact.html': 'delpoy/contact.html',     // 'destination': 'source'
+      'src/about.html': 'delpoy/contact.html',     // 'destination': 'source'
+      'src/404.html': 'deploy/404.html',     // 'destination': 'source'
     }
   }
 },
  uglify: {
     my_target: {
       files: {
-        'src/assets/js/<%= pkg.name %>.js': ['src/assets/js/<%= pkg.name %>.js']
+        'src/js/script.js': ['deploy/js/script.js']
       }
     }
   },
 jshint: {
-  files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+  files: ['Gruntfile.js', 'src/**/*.js'],
   options: {
     // options here to override JSHint defaults
     globals: {
@@ -140,8 +139,8 @@ watch: {
 
 });
 
-grunt.registerTask('deploy', ['aws_s3:live']);
-grunt.registerTask('download', ['aws_s3:download']);
-grunt.registerTask('default', ['concat', 'uglify','cssmin','imagemin']);
+// grunt.registerTask('deploy', ['aws_s3:live']);
+// grunt.registerTask('download', ['aws_s3:download']);
+grunt.registerTask('default', ['concat', 'uglify','cssmin', 'htmlmin']);
 
 };
